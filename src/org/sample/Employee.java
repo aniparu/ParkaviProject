33ac65b3dd388f085d134c3e1c4f69f9144f10f6
aniparu <aniparu122@gmail.com>
2022-10-28 15:35:08 +0530

    This is my project

---
+++ src/org/sample/Employee.java
@@ -0,0 +1,17 @@
+package org.sample;
+
+public class Employee {
+	
+	private void employeeId() {
+		System.out.println("23456789");
+
+	}
+	
+	public static void main(String[] args) {
+		
+		Employee  e = new Employee();
+		e.employeeId();
+		
+	}
+
+}
